0671c32f76d218858abc1d888fa9412c087f1ed9
krvavych <natalia.krvavych@gmail.com>
2014-10-29 12:45:09 +0200

    #3 create class Graphe wich have fialds vertexes and edges

--- SearchAlgorythms/src/fielden/com/ua/search/Graph.java
+++ SearchAlgorythms/src/fielden/com/ua/search/Graph.java
@@ -1,5 +1,22 @@
 package fielden.com.ua.search;
 
+import java.util.List;
+
 public class Graph {
+	final private List<Vertex> verteces;
+	final private List<Edge> edges;
+	
+	public Graph (List<Vertex> verteces, List<Edge> edges){
+		this.verteces = verteces;
+		this.edges = edges;
+	}
+
+	public List<Vertex> getVerteces() {
+		return verteces;
+	}
+
+	public List<Edge> getEdges() {
+		return edges;
+	}
 
 }
