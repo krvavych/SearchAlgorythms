c8482a26a8106398fb6b40b98269ad988597701e
krvavych <natalia.krvavych@gmail.com>
2014-10-29 12:27:00 +0200

    #3 create class Edge

---
+++ SearchAlgorythms/src/fielden/com/ua/search/Edge.java
@@ -0,0 +1,31 @@
+package fielden.com.ua.search;
+
+public class Edge {
+	final private Integer id;
+	final private Vertex sourse;
+	final private Vertex destination;
+	
+	public Edge( Integer id, Vertex sourse, Vertex destination){
+		this.id = id;
+		this.sourse = sourse;
+		this.destination = destination;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public Vertex getSourse() {
+		return sourse;
+	}
+
+	public Vertex getDestination() {
+		return destination;
+	}
+
+	@Override
+	public String toString() {
+		return  id +" "+ sourse +" "+ destination;
+	}
+
+}
